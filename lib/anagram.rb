8657e8556bc7f59282f1bee6b3e9c1b52e8d87be
Dark-Raine <anderson-tomas@hotmail.com>
2018-12-16 11:11:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :string
+
+    def initialize(string)
+        @string = string
+    end
+
+    def match(tomatch)
+        initword = @string.split("")
+        initword.sort!
+        find = []
+        matched = []
+        tomatch.each do |word|
+            find = word.split("")
+            find.sort!
+            
+            if initword == find
+                matched << word
+                
+                
+            end
+            
+
+        end
+        matched
+    end
+
+
+end
